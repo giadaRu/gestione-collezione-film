ab04daa641a128a8b8c3916b5c4b755e96089178
Giada <giadarusso60gmail.com>
2025-10-26 10:58:59 +0100

    Pulizia vecchi file e aggiunta nuova struttura Maven con test

---
+++ gestione-videoteca/src/main/java/it/unical/videoteca/App.java
@@ -0,0 +1,13 @@
+package it.unical.videoteca;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
